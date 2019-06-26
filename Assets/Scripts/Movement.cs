6c380dbc6713b9deaa8d289ec162b22381a0ea66
Leon Macleod <leondeanmacleod@gmail.com>
2019-06-27 11:54:06 +1200

    Merged AI Code, cleaned up files, started on raycast teleport.

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.AI;
+public class Movement : MonoBehaviour
+{
+
+    public UnityEvent UIR;
+    UnityEvent shouldPath;
+    public Transform goal;
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+        
+
+
+
+    }
+
+    public void moveAgent()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UserInteractionRange.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UserInteractionRange : MonoBehaviour
+{
+    public Transform eventPosition;
+    public UnityEvent InteractionRange;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if(InteractionRange == null)
+        {
+            InteractionRange = new UnityEvent();
+           
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+
+        Vector3 playerPosition = this.transform.position;
+        float distance = Vector3.Distance(playerPosition, eventPosition.position);
+        if (distance < 5.0f)
+        {
+
+
+            InteractionRange.Invoke();
+            Debug.Log("Invoked Reaction Range");
+        }
+
+        Debug.Log("distance:" + distance);
+        
+
+    }
+}
