02b4deeda2558c049f636309abfdcb16d84d3d75
Leon Macleod <leondeanmacleod@gmail.com>
2019-06-27 13:39:42 +1200

    Basic Teleportation

--- Assets/Scripts/RayCastTeleport.cs
+++ Assets/Scripts/RayCastTeleport.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class RayCastTeleport : MonoBehaviour
 {
+    public GameObject player;
+    private LineRenderer thisLR;
     // Start is called before the first frame update
     void Start()
     {
+
+
+        thisLR = GetComponent<LineRenderer>();
+
         
     }
 
@@ -15,9 +21,38 @@ public class RayCastTeleport : MonoBehaviour
     {
         RaycastHit hit;
 
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 25f)){
+        int layerMask = 1 << 9;
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask)){
+
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("RayHit");
+
+            thisLR.SetPosition(0, this.transform.position);
+            thisLR.SetPosition(1, hit.point);
+            Debug.Log(hit.point.ToString());
+
+
+
+            if (Input.GetButtonDown("RightTriggerPress"))
+            {
+                Debug.Log("GOT HERE");
+                player.transform.position = hit.point;
+            }
+
+
+            
+
+
+
 
         }
+        else
+        {
+            thisLR.SetPositions(new Vector3[] { Vector3.zero, Vector3.zero });
+        }
+
+        
 
         
     }
